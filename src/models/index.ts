8035dc56ca720b971bda215f22b0d82d4050352a
bienvenue cedrick <cedrick4smith@gmail.com>
2025-10-09 20:27:43 +0200

    feat(models): create Trip model with derived features

---
+++ src/models/index.ts
@@ -0,0 +1,14 @@
+import Trip from './Trip';
+
+// Export all models
+export { Trip };
+
+// Initialize model associations (if we had multiple models)
+export const initializeModels = (): void => {
+  // If we had relationships, we'd define them here
+  // Example: User.hasMany(Trip);
+};
+
+export default {
+  Trip,
+};
