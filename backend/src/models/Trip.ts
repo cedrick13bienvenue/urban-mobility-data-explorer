5d1b92c809e4cba73947c16132ef9e57b5cd0970
Byiringiro Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2025-10-16 09:59:44 +0200

    separating frontend and backend

--- backend/src/models/Trip.ts
+++ backend/src/models/Trip.ts
@@ -1,5 +1,5 @@
-import { DataTypes, Model, Optional } from 'sequelize';
-import sequelize from '../config/database';
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/database";
 
 // Trip attributes interface matching the actual dataset
 interface TripAttributes {
@@ -15,7 +15,7 @@ interface TripAttributes {
   dropoffLatitude: number;
   storeAndFwdFlag: string;
   tripDuration: number; // Trip duration in seconds from dataset
-  
+
   // Derived features
   tripDurationMinutes: number;
   tripDistance: number; // Calculated using Haversine formula
@@ -26,16 +26,19 @@ interface TripAttributes {
   monthOfYear: number;
   isRushHour: boolean;
   tripCategory: string; // 'short', 'medium', 'long' based on distance
-  
+
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 // For creation, id is optional
-interface TripCreationAttributes extends Optional<TripAttributes, 'id'> {}
+interface TripCreationAttributes extends Optional<TripAttributes, "id"> {}
 
 // Trip model class
-class Trip extends Model<TripAttributes, TripCreationAttributes> implements TripAttributes {
+class Trip
+  extends Model<TripAttributes, TripCreationAttributes>
+  implements TripAttributes
+{
   public id!: number;
   public tripId!: string;
   public vendorId!: number;
@@ -48,7 +51,7 @@ class Trip extends Model<TripAttributes, TripCreationAttributes> implements Trip
   public dropoffLatitude!: number;
   public storeAndFwdFlag!: string;
   public tripDuration!: number;
-  
+
   public tripDurationMinutes!: number;
   public tripDistance!: number;
   public tripSpeedKmh!: number;
@@ -58,7 +61,7 @@ class Trip extends Model<TripAttributes, TripCreationAttributes> implements Trip
   public monthOfYear!: number;
   public isRushHour!: boolean;
   public tripCategory!: string;
-  
+
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 }
@@ -75,122 +78,122 @@ Trip.init(
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      field: 'trip_id',
+      field: "trip_id",
     },
     vendorId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'vendor_id',
+      field: "vendor_id",
     },
     pickupDatetime: {
       type: DataTypes.DATE,
       allowNull: false,
-      field: 'pickup_datetime',
+      field: "pickup_datetime",
     },
     dropoffDatetime: {
       type: DataTypes.DATE,
       allowNull: false,
-      field: 'dropoff_datetime',
+      field: "dropoff_datetime",
     },
     passengerCount: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'passenger_count',
+      field: "passenger_count",
     },
     pickupLongitude: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'pickup_longitude',
+      field: "pickup_longitude",
     },
     pickupLatitude: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'pickup_latitude',
+      field: "pickup_latitude",
     },
     dropoffLongitude: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'dropoff_longitude',
+      field: "dropoff_longitude",
     },
     dropoffLatitude: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'dropoff_latitude',
+      field: "dropoff_latitude",
     },
     storeAndFwdFlag: {
       type: DataTypes.STRING(1),
       allowNull: false,
-      field: 'store_and_fwd_flag',
+      field: "store_and_fwd_flag",
     },
     tripDuration: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'trip_duration',
+      field: "trip_duration",
     },
-    
+
     // Derived features
     tripDurationMinutes: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'trip_duration_minutes',
+      field: "trip_duration_minutes",
     },
     tripDistance: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'trip_distance',
+      field: "trip_distance",
     },
     tripSpeedKmh: {
       type: DataTypes.FLOAT,
       allowNull: false,
-      field: 'trip_speed_kmh',
+      field: "trip_speed_kmh",
     },
     hourOfDay: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'hour_of_day',
+      field: "hour_of_day",
     },
     dayOfWeek: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'day_of_week',
+      field: "day_of_week",
     },
     isWeekend: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      field: 'is_weekend',
+      field: "is_weekend",
     },
     monthOfYear: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      field: 'month_of_year',
+      field: "month_of_year",
     },
     isRushHour: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      field: 'is_rush_hour',
+      field: "is_rush_hour",
     },
     tripCategory: {
       type: DataTypes.STRING(20),
       allowNull: false,
-      field: 'trip_category',
+      field: "trip_category",
     },
   },
   {
     sequelize,
-    tableName: 'trips',
+    tableName: "trips",
     indexes: [
-      { fields: ['trip_id'], unique: true },
-      { fields: ['pickup_datetime'] },
-      { fields: ['dropoff_datetime'] },
-      { fields: ['trip_distance'] },
-      { fields: ['trip_duration'] },
-      { fields: ['hour_of_day'] },
-      { fields: ['day_of_week'] },
-      { fields: ['is_weekend'] },
-      { fields: ['month_of_year'] },
-      { fields: ['is_rush_hour'] },
-      { fields: ['vendor_id'] },
-      { fields: ['trip_category'] },
+      { fields: ["trip_id"], unique: true },
+      { fields: ["pickup_datetime"] },
+      { fields: ["dropoff_datetime"] },
+      { fields: ["trip_distance"] },
+      { fields: ["trip_duration"] },
+      { fields: ["hour_of_day"] },
+      { fields: ["day_of_week"] },
+      { fields: ["is_weekend"] },
+      { fields: ["month_of_year"] },
+      { fields: ["is_rush_hour"] },
+      { fields: ["vendor_id"] },
+      { fields: ["trip_category"] },
     ],
   }
 );
